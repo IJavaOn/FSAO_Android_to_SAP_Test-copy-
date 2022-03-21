607261cfad27bd8dbfc537a0128b6d91d21dbe37
shani <shanisharma135@gamil.com>
2022-03-21 16:08:18 +0530

    FSAO_Test Android

--- app/src/main/java/com/sap/copy/mdui/Test2.java
+++ app/src/main/java/com/sap/copy/mdui/Test2.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.sap.cloud.android.odata.eam_ntf_create_entities.MyNotification;
 import com.sap.copy.R;
+import com.sap.copy.mdui.mynotificationset.MyNotificationSetActivity;
 
-public class Test2 extends AppCompatActivity {
+public class Test2 extends MyNotificationSetActivity implements InterfacedFragment.InterfacedFragmentListener<MyNotification> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
